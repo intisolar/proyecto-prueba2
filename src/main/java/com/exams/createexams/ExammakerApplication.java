d63c35d6d195621ec5944fdf64da09f2b70dd72d
intisolar <insolgar@gmail.com>
2022-03-27 18:15:10 -0300

    Initial commit

---
+++ src/main/java/com/exams/createexams/ExammakerApplication.java
@@ -0,0 +1,13 @@
+package com.exams.createexams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExammakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExammakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exams/createexams/ExammakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exams.createexams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExammakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
