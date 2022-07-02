b2d7e4304278d2504bc45cbde750dffb998d9fc0
Gomez Simon <saaimonsg@gmail.com>
2022-07-02 02:53:02 -0300

    Student crud with 3 endpoints.
    Missing security for requests

---
+++ src/main/java/com/exams/createexams/exception/student/StudentException.java
@@ -0,0 +1,12 @@
+package com.exams.createexams.exception.student;
+
+public class StudentException extends Exception {
+
+    public StudentException() {
+    }
+
+    public StudentException(String message) {
+        super(message);
+    }
+
+}
